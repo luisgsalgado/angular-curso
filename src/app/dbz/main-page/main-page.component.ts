22f712b1d73ae18f908bb761cee864c5f863170a
Luis Salgado <luguisasa92@gmail.com>
2022-03-25 14:21:41 -0500

    first commit

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent  {
 
-  constructor() { }
+  personajes: Personaje [] = [
+      {
+        nombre: 'Goku',
+        poder: 16000
+      },
+      {
+        nombre: 'Vegeta',
+        poder: 9000
+      }
 
-  ngOnInit(): void {
+  ]
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  agregar(){
+
+    if ( this.nuevo.nombre.trim().length ===0){
+      return;
+    }
+
+    
+    console.log ( this.nuevo);
+
+     this.personajes.push( this.nuevo);
+     this.nuevo = {
+       nombre:'',
+       poder: 0
+     }
   }
 
 }
